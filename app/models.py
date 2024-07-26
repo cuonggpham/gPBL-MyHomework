b186ac5e3c9a121ba172e60864661400da68a2d7
al23029 <al23029@shibaura-it.ac.jp>
2024-07-26 15:02:04 +0900

    view-homework & finished-homework will display only homework that we put from admin

--- app/models.py
+++ app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Homework(models.Model):
+    name = models.CharField(max_length=200) #宿題名
+    note = models.TextField(blank=True, default='') #メモ
+    due_date = models.DateField() #期限
+    difficulty = models.IntegerField(choices=[(1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5')]) #難易度
+    priority = models.IntegerField(default=0, editable=False) #自動的に計算する優先度
+    finished = models.BooleanField(default=False) #完了した宿題かどうか
+
+    def save(self, *args, **kwargs):
+        # priorityの計算、(入力された期限ー今の時間)＊(５－入力された難易度)
+        now = timezone.now()
+        time_difference = (self.due_date - now.date()).days
+        self.priority = time_difference * (5 - self.difficulty)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        #管理画面で宿題の名前が表示されるように設定している
+        return self.name
--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,25 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
 
 
 urlpatterns = [
+    #path to see admin screen 管理画面へのパス
+    path('admin/', admin.site.urls),
+
     path('', views.home),
 
     path('view/sign-in.html', views.sign_in),
 
-    path('view/sign-up.html', views.sign_up)
+    path('view/sign-up.html', views.sign_up),
+
+    path('view/index-logined.html', views.index_logined),
+
+    path('view/view-homework.html', views.view_homework, name='view_homework'),
+
+    path('view/add-homework.html', views.add_homework),
+
+    path('view/homework-details.html/<int:id>/', views.homework_details, name='homework_details'),
+
+    path('view/finished-homework.html', views.finished_homework, name='finished_homework')
 ]
