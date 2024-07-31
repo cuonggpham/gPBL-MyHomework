76ae4d5acd8ffa45792a84dc3c77909252dc8f3d
al23029 <al23029@shibaura-it.ac.jp>
2024-07-31 23:35:35 +0900

    static files setting, new

--- homework_management/settings.py
+++ homework_management/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l$tq@h%j@452l9&9+9j^1bs&-xyrcmvp-)kfc$r(0s5g*!3ao_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-ALLOWED_HOSTS = ['localhost', '.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 LOGIN_URL = 'sign_in'
 
@@ -119,7 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
